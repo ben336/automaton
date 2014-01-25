4277d5092a0e3ead88401cea37d9362ddec6461e
Ben McCormick <ben.mccormick@alumni.duke.edu>
2014-01-24 23:22:31 -0500

    Got it passing the simple test case. Tomorrow I can clean up the code and make it pretty

--- src/Automaton.js
+++ src/Automaton.js
@@ -1,11 +1,35 @@
 
 define(function() {
 
+    /* Creates an array representing the binary state of the
+     * number.  Each index is the 2^i component of the original 
+     * number.  The array assumes a positive integer in the range
+     * 0-255.  It will fail with other input
+     */
+    function defineBinaryArray(num) {
+      var i=7,end =0,arr = []; 
+      for(; i>=end; i--) {
+        arr[i] = Math.floor(num/Math.pow(2,i));
+        num -= arr[i]*Math.pow(2,i);
+      }
+      return arr;
+    }
+  
     return {
 
       translate: function(input,rule) {
-        return input.map(function(block, index,original){
-          
+        var expandedInput = [0].concat(input,0),
+            outputMap= defineBinaryArray(rule);
+        
+        return expandedInput.map(function(block, index,original){
+          var inputs = [
+            (index > 0 ? original[index-1] : 0),
+            original[index],
+            (index === original.length -1 ? 0 : original[index+1])
+          ]; 
+        
+          return outputMap[4*inputs[0]+2*inputs[1]+inputs[2]];
+
         });
       }
 

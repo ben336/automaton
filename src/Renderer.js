bed834d9b7dd83c02448eb564afc62df2a6e3ba8
Ben McCormick <ben.mccormick@alumni.duke.edu>
2014-01-25 22:54:35 -0500

    Added D3 to make it pretty.  Example is basically working as is.  Renderer now is the only object holding the state of the entire chart, the init file just holds the current phases state

--- src/Renderer.js
+++ src/Renderer.js
@@ -1,24 +1,73 @@
-define(function() {
+define(["lib/d3"],function(d3) {
 
+  var svg, x, y, dataset = [],blockHeight,blockWidth,max,
+  viewWidth =  document.documentElement.clientWidth || 800,
+  viewHeight = document.documentElement.clientHeight || 800;
+  
   var Renderer = {
-    
-    render: function(rows,target) {
-      target.innerHTML = toStr(rows);
+   
+    init: function(target,numIterations){
+
+      var enter,
+          margin = {top: 40, right: 20, bottom: 40, left: 20},
+          width = viewWidth- margin.left - margin.right,
+          height = viewHeight - margin.top - margin.bottom;
+
+      x = d3.scale.linear()
+          .range([0,width])
+          .domain([0-numIterations,numIterations]);
+
+      y = d3.scale.linear()
+          .range([0,height])
+          .domain([0,numIterations]);
+      
+      blockHeight = height/numIterations;
+      blockWidth = width/(numIterations*2+1);
+      //stop after the max iterations
+      max = numIterations;
+      // An SVG element with a bottom-right origin.
+      svg = d3.select(target).append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom);
+    },
+
+    render: function(row,iteration) {
+      if(iteration > max) {
+        //if we're not going to be able to see it, don't do anything
+        return;
+      }
+      var dataPoints = row.map(function(point,index,fullrow) {
+        return {
+          x: index - Math.floor(fullrow.length/2),
+          y: iteration,
+          color: point === 1 ? "#2d578b" : "#FCFCFC"
+        }
+      });
+      dataset = dataset.concat(dataPoints);
+      svg.selectAll("rect")
+        .data(dataset)
+        .enter()
+        .append("svg:rect")
+        .attr("x", function(d) { 
+          return x(d.x);
+        })
+        .attr("y", function(d) { 
+          return y(d.y)
+        })
+        .attr("height",blockHeight)
+        .attr("width",blockWidth)
+        .attr("fill",function(d) { 
+          return d.color 
+        });
     },
 
     clear: function(target) {
+      dataset = [];
       target.innerHTML = "";
     }
 
   }
  
-  // temp function to translate rows to an html string
-  function toStr(rows) {
-    var spans = rows.map(function(row) {
-      return "<span> [" + row.join(" , ") + " ] </span>";
-    });
-    return spans.join("<br>");
-  }
 
   return Renderer;
 
--- src/init.js
+++ src/init.js
@@ -1,9 +1,16 @@
+requirejs.config({
+  "lib/d3": {
+    exports:["d3"]
+  }
 
+});
 require( ["src/Automaton",
           "src/Renderer"],
 
 function(Automaton,Renderer){
-  var rows = [[1]],
+  var row = [1],
+      iteration = 0,
+      maxiteration = 50,
       startBtn = document.getElementById("startbtn"),
       stopBtn = document.getElementById("stopbtn"),
       content = document.getElementById("content"),
@@ -11,19 +18,32 @@ function(Automaton,Renderer){
       interval;
   
   startBtn.addEventListener("click",function() {
-   interval = setInterval(drawAndCalculate,2000); 
+    //don't want to do anything if not setup
+    if(iteration>0) { 
+      return false;
+    }
+    Renderer.clear(content);
+    row= [1];
+    iteration = 1;
+    Renderer.init(content,maxiteration);
+    interval = setInterval(drawAndCalculate,2000); 
   });
 
   stopBtn.addEventListener("click",function() {
+    iteration = 0;
     clearInterval(interval);
-    rows = [[1]];
-    Renderer.clear(content)
   });
 
   function drawAndCalculate() {
     //draw first, then calculate, so that we're ready to draw right away
-    Renderer.render(rows,content);
-    rows.push(Automaton.translate(rows[rows.length-1],rule));
+    if(iteration > maxiteration) {
+      iteration = 0;
+      clearInterval(interval);
+      return;
+    }
+    Renderer.render(row,iteration);
+    row = Automaton.translate(row,rule);
+    iteration++;
   }
 
 });
